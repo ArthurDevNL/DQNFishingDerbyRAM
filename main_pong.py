6cf6579a9139955de1e5801faf689109be6a1af0
Arthur Hemmer <arthurhemmer@hotmail.com>
2018-03-16 15:44:36 +0100

    Changed pong to reproduce paper

--- main_pong.py
+++ main_pong.py
@@ -16,55 +16,49 @@ env.seed(42)
 test = False
 load_model = test
 
+m = {0:0, 1:3, 2:4}
+
 observation = env.reset()
 state_size = observation.shape[0]
 
 
-actions = [0,1,2,3]
-n_actions = 4 #env.action_space.n
+actions = [0,1,2]
+n_actions = 3 #env.action_space.n
 
 # Initialize dataset D
-D = deque(maxlen=20000)
+D = deque(maxlen=100000)
 
 # Initialize value function
 model = Sequential()
-model.add(Dense(256, input_dim=state_size, activation='relu'))
+model.add(Dense(128, input_dim=state_size, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(n_actions))
 
-opt = RMSprop(lr=-.0001)
+opt = RMSprop(lr=0.0002)
 model.compile(loss='mse', optimizer=opt)
-
-if load_model:
-	json_file = open('model.json', 'r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	model = model_from_json(loaded_model_json)
-	model.load_weights("model.h5")
-	model.compile(loss='mse', optimizer='adam')
 
 def phi(x):
 	return x / 255.0
 
 e = 1.0 if not test else 0.1
-e_decay = 0.995
-e_min = 0.01
+e_decay_frames = 1000000
+e_min = 0.1
 
-gamma = 0.99
+gamma = 0.95
 
-update_freq = 100
+update_freq = 10
 counter = 0
 
-for episode in range(1000):
+replay_mem_size = 100
+
+episode = 0
+while True:
 	observation = env.reset()
 
 	total_catch_value = 0
 	done = False
-	# for t in range(2000):
 	while not done:
 		env.render()
 
@@ -75,7 +69,7 @@ for episode in range(1000):
 
 		# Take a random action fraction e (epsilon) of the time
 		action = None
-		if np.random.rand() <= e:
+		if np.random.rand() <= e or counter < replay_mem_size:
 			action = np.random.choice(range(n_actions))
 			# action = env.action_space.sample()
 		else:
@@ -93,7 +87,7 @@ for episode in range(1000):
 		# reward = max(0, reward)
 		# if reward == 0:
 		# 	reward = -0.01
-		#
+		# #
 		# if reward > 0:
 		# 	reward *= 2
 
@@ -104,27 +98,41 @@ for episode in range(1000):
 		observation = observation_
 
 		# Train the Q function
-		if not test and counter % update_freq == 0 and len(D) > 32:
+		if counter > replay_mem_size and not test and counter % update_freq == 0 and len(D) > 32:
 			# Train the model
 			batch_idxs = np.random.choice(range(len(D)), 32)
 			batch = [D[i] for i in batch_idxs]
+
+			X = []
+			ys = []
 			for s, a, r, s_, d in batch:
 
 				y = r
 				if not d:
 					y = r + gamma * np.amax(model.predict(s_.reshape(1,state_size))[0])
 
-				# target_f = model.predict(s.reshape(1,state_size))
-				target_f = np.zeros((1,4))
+				target_f = model.predict(s.reshape(1,state_size))
 				target_f[0][a] = y
-				model.fit(s.reshape(1,state_size), target_f, epochs=1, verbose=0)
+				ys.append(target_f)
+				X.append(s)
+
+			X = np.array(X)
+			model.fit(X, np.array(ys).reshape(32, n_actions), epochs=1, verbose=0)
 
 		counter += 1
 
-	print('Finished episode', episode, total_catch_value, e)
+		if e > e_min and counter > replay_mem_size:
+			e -= (1.0 - e_min) / e_decay_frames
+			e = max(e_min, e)
 
-	if e > e_min:
-		e *= e_decay
+	print('Finished episode', episode, total_catch_value, counter, e)
 
+	if episode % 20 == 0:
+		model_json = model.to_json()
+		with open("model_breakout.json", "w") as json_file:
+			json_file.write(model_json)
+		model.save_weights("model_breakout.h5")
+
+	episode += 1
 
 env.close()
