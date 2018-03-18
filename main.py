e0deb9af926af9c8808d1473024e7f5f2f67b76f
Arthur Hemmer <arthurhemmer@hotmail.com>
2018-03-18 17:02:51 +0100

    New state input, flattening and breakout reward discount

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import gym
 from keras.models import Sequential, model_from_json
 from keras.optimizers import RMSprop
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import *
 from collections import deque
+from itertools import islice
 import numpy as np
 from time import sleep
-env = gym.make('Breakout-ram-v4')
+env = gym.make('Breakout-ram-v0')
 env.seed(42)
 
 # Logging and monitoring
@@ -24,25 +25,21 @@ n_actions = 4 #env.action_space.n
 
 print(env.unwrapped.get_action_meanings())
 
-# Initialize dataset D
-D = deque(maxlen=100000)
-
 hist_size = 4
 
 # Initialize value function
 model = Sequential()
-model.add(Flatten(input_shape=(hist_size, state_size)))
+model.add(Flatten(input_shape=(state_size, hist_size)))
+model.add(Lambda(lambda x: x / 255.0))
 model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
 model.add(Dense(n_actions))
 
-opt = RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+opt = RMSprop(lr=0.0001, rho=0.95, epsilon=0.01)
 model.compile(loss='mse', optimizer=opt)
 
-def phi(x):
-	return x / 255.0
+# Initialize dataset D
+D = deque(maxlen=100000)
 
 e = 1.0 if not test else 0.1
 e_decay_frames = 1000000
@@ -53,78 +50,86 @@ gamma = 0.99
 update_freq = 4
 counter = 0
 
-replay_mem_size = 50000
+# replay_mem_size = 50000
+replay_mem_size = 1000
+batch_size = 32
 
 episode = 0
-
-history = deque(maxlen=hist_size)
 while True:
-	observation = env.reset()
+	state = env.reset()
 
 	total_catch_value = 0
 	done = False
 	while not done:
-		# env.render()
+		env.render()
 
 		if test:
 			sleep(0.01)
-
-		state = phi(observation)
-		history.append(state)
 
 		# Take a random action fraction e (epsilon) of the time
 		action = None
 		if np.random.rand() <= e or counter < replay_mem_size:
 			action = np.random.choice(range(n_actions))
-			# action = env.action_space.sample()
 		else:
-			q_values = model.predict(np.array(history).reshape(1, hist_size, state_size))
+			sl = list(islice(D, len(D) - (hist_size - 1), len(D)))
+			prev_states = [x[0] for x in sl]
+			prev_states.append(state)
+			stack = np.stack(prev_states, axis=1)
+			q_values = model.predict(stack.reshape(1, state_size, hist_size))
 			action = q_values[0].argsort()[-1]
 
 			if test:
 				print(action)
 
 		# Take the chosen action
-		observation_, reward, done, info = env.step(action)
+		state_, reward, done, info = env.step(actions[action])
 		if reward > 0:
 			total_catch_value += reward
 
 		# reward = max(0, reward)
-		# if reward == 0:
-		# 	reward = -0.001
+		if reward == 0:
+			reward = -0.01
+
 		# #
 		# if reward > 0:
 		# 	reward *= 2
 
 		# Store the tuple
-		state_ = phi(observation_)
-		if len(history) == 4:
-			D.append((np.array(history).reshape(hist_size, state_size), action, reward, state_, done))
+		D.append((state, action, reward, state_, done))
 
-		observation = observation_
+		state = state_
 
 		# Train the Q function
-		if counter > replay_mem_size and not test and counter % update_freq == 0 and len(D) > 32:
+		if counter > replay_mem_size and not test and counter % update_freq == 0 and len(D) > (batch_size + hist_size):
+			D_ = list(D)
+
 			# Train the model
-			batch_idxs = np.random.choice(range(len(D)), 32)
-			batch = [D[i] for i in batch_idxs]
+			batch_idxs = np.random.choice(range(hist_size, len(D_)), batch_size)
 
 			X = []
 			ys = []
-			for s, a, r, s_, d in batch:
+			for i in batch_idxs:
+				s, a, r, s_, d = D_[i]
 
 				y = r
 				if not d:
-					s_ = np.concatenate([s,s_.reshape(1,128)], axis=0)[1:].reshape(1, hist_size, state_size)
-					y = r + gamma * np.amax(model.predict(s_)[0])
+					states_ = [x[3] for x in D_[i-(hist_size - 1):i]]
+					states_.append(s_)
+					stack_ = np.stack(states_, axis=1).reshape(1, state_size, hist_size)
+					y = r + gamma * np.amax(model.predict(stack_)[0])
 
-				target_f = model.predict(s.reshape(1, hist_size,state_size))
+				states = [x[0] for x in D_[i-(hist_size-1):i]]
+				states.append(s)
+				stack = np.stack(states, axis=1).reshape(1, state_size, hist_size)
+				X.append(stack)
+
+				# Calculate the target vector
+				target_f = model.predict(stack)
 				target_f[0][a] = y
 				ys.append(target_f)
-				X.append(s)
 
-			X = np.array(X)
-			model.fit(X, np.array(ys).reshape(32, n_actions), epochs=1, verbose=0)
+			X = np.array(X).reshape(batch_size, state_size, hist_size)
+			model.fit(X, np.array(ys).reshape(batch_size, n_actions), epochs=1, verbose=0)
 
 		counter += 1
 
@@ -134,10 +139,10 @@ while True:
 
 	print('Finished episode', episode, total_catch_value, counter, e)
 
-	if episode % 20 == 0:
-		model_json = model.to_json()
-		with open("model.json", "w") as json_file:
-			json_file.write(model_json)
-		model.save_weights("model.h5")
+	# if episode % 20 == 0:
+	# 	model_json = model.to_json()
+	# 	with open("model.json", "w") as json_file:
+	# 		json_file.write(model_json)
+	# 	model.save_weights("model.h5")
 
 	episode += 1
