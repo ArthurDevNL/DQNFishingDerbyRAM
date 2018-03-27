7a8da07e957c5d5565ffe83e52dcb662309b1f31
Arthur Hemmer <arthurhemmer@hotmail.com>
2018-03-27 13:43:19 +0200

    low lr

--- main_manual_states_stack.py
+++ main_manual_states_stack.py
@@ -94,7 +94,7 @@ def huber_loss(a, b, in_keras=True):
 		use_linear_term = K.cast(use_linear_term, 'float32')
 	return use_linear_term * linear_term + (1-use_linear_term) * quadratic_term
 
-opt = RMSprop(lr=0.001)
+opt = RMSprop(lr=0.0001)
 model.compile(loss=huber_loss, optimizer=opt)
 
 # Initialize dataset D
