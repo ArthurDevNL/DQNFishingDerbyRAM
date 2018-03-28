915a885fe19e43e53493729a13c4f67bfb00a0d2
Arthur Hemmer <arthurhemmer@hotmail.com>
2018-03-28 09:19:48 +0200

    linex and y features

--- main_manual_states_stack.py
+++ main_manual_states_stack.py
@@ -48,10 +48,10 @@ def phi(x):
 	xclip = 20
 	# v2 = fish2_top_x - line_x
 	# v2y = line_y - 217
-	v3 = fish4_top_x - line_x
-	v3y = line_y - 230
-	v4 = fish6_top_x - line_x
-	v4y = line_y - 245
+	# v3 = fish4_top_x - line_x
+	# v3y = line_y - 230
+	# v4 = fish6_top_x - line_x
+	# v4y = line_y - 245
 
 	shark_x = int(x[75])
 	# shark_y = 213
@@ -61,7 +61,10 @@ def phi(x):
 
 	caught_fish_idx = 112
 	v0 = int(x[caught_fish_idx])
-	return np.array([v0, v3, v3y, v4, v4y])
+
+	v1 = int(line_x) - 20
+	v2 = 245 - int(line_y)
+	return np.array([v0, v1, v2])
 
 observation = env.reset()
 state_size = phi(observation).shape[0]
@@ -80,8 +83,8 @@ hist_size = 1
 # Initialize value function
 model = Sequential()
 model.add(Flatten(input_shape=(state_size, hist_size)))
-model.add(Dense(8))
-model.add(Dense(8))
+model.add(Dense(32))
+model.add(Dense(32))
 model.add(Dense(n_actions))
 
 print(model.summary())
@@ -111,15 +114,15 @@ model.compile(loss=huber_loss, optimizer=opt)
 D = deque(maxlen=500000)
 
 e = 1.0 if not test else 0.05
-e_decay_frames = 300000
+e_decay_frames = 100000
 e_min = 0.05
 
 gamma = 0.99
 
-update_freq = 1
+update_freq = 64
 counter = 0
 
-min_replay_mem_size = 50000
+min_replay_mem_size = 10000
 batch_size = 32
 
 pending_reward_idx = 114
